7247dba1303e236652793aee7b63588f478397f0
Yola <i.yalovecky@gmail.com>
2015-12-06 19:17:38 +0200

    Wordizer

--- ClassOne/ClassOne.cpp
+++ ClassOne/ClassOne.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main()
 {
+    string words;
+    cout << "Please type a sentence" << endl;
+    getline(cin, words);
+    
+    for (int i = 0; i < words.size(); ++i)
+    {
+        if (words[i] != ' ')
+            cout << words[i];
+        else
+            cout << endl;
+    }
+
     return 0;
 }
 
