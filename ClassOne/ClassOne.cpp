628cd286a4838f3dcbbf08ea038eb8de260af0cd
Yola <i.yalovecky@gmail.com>
2015-12-26 17:30:17 +0200

    added square matrix multiplication

--- ClassOne/ClassOne.cpp
+++ ClassOne/ClassOne.cpp
@@ -13,7 +13,7 @@ void wordizer()
     cout << "Please type a sentence" << endl;
     getline(cin, words);
 
-    for (int i = 0; i < words.size(); ++i)
+    for (size_t i = 0; i < words.size(); ++i)
     {
         if (words[i] != ' ')
             cout << words[i];
@@ -26,16 +26,20 @@ void wordizer()
 
 const int Dim = 3;
 
-void matrisProd(int a[Dim][Dim], int b[Dim][Dim], int (&c)[Dim][Dim])
+void matrisProd(int a[Dim][Dim], int b[Dim][Dim], int c[Dim][Dim])
 {
-    int matrisProdSum = 0;
+  
     for (int i = 0; i < Dim; i++)
     {
         for (int j = 0; j < Dim; j++)
         {
-            c[i][j]= matrisProdSum + a[i][j] * b[j][i];
+            c[i][j] = 0;
+
+            for (int k = 0; k < Dim; k++)
+            {
+                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+            }
         }
-        
     }
     return;
 }
@@ -72,8 +76,21 @@ int main()
     int vV[Dim] = { 4,2,3 };
     cout << dotProd(v, vV) << endl;
 
-    int matrisIn[Dim][Dim] = {{1,2,3},{1,2,4},{1,2,5}};
-    matrisOut(matrisIn);
+    int matrisIn1[Dim][Dim] = { {1,2,3},{1,2,4},{1,2,5} };
+    int matrisIn2[Dim][Dim] = { {1,0,0},{0,1,0},{0,0,1} };
+
+    int matrixProdResult[Dim][Dim];
+    
+    matrisProd(matrisIn1, matrisIn2, matrixProdResult);
+
+    matrisOut(matrisIn1);
+    cout << endl;
+
+    matrisOut(matrisIn2);
+    cout << endl;
+
+    matrisOut(matrixProdResult);
+    cout << endl;
 
     cin.get();
     return 0;
